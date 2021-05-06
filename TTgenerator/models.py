03699ca15b362f225f8f7604a2949e18fee5609d
chris9515 <pamulachris@gmail.com>
2021-05-06 15:59:28 +0530

    profile page updated

--- TTgenerator/models.py
+++ TTgenerator/models.py
@@ -10,10 +10,13 @@ class Student(models.Model):
     # name = models.CharField(max_length=20)
     year = models.DateField(auto_now_add=True)
     # email = models.EmailField()
+    def __str__(self):
+        return self.user.username
     
 class Teacher(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher')
     # name = models.CharField(max_length=20)
     # email = models.EmailField()
     subject = models.CharField(max_length = 30, default="Maths")
-
+    def __str__(self):
+        return self.user.username
